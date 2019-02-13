d43337972fe4207690aa063c62bcbf37ba3dec9c
massodasuki <massodasuki@gmail.com>
2019-02-13 13:15:54 +0800

    7
    7

--- app/src/main/java/com/unifreelancer/signup/UserProfile.java
+++ app/src/main/java/com/unifreelancer/signup/UserProfile.java
@@ -1,4 +1,22 @@
 package com.unifreelancer.signup;
 
 public class UserProfile {
+
+    private String header;
+
+    private String profileContent;
+
+    public UserProfile(String header, String profileContent) {
+        this.header = header;
+        this.profileContent = profileContent;
+    }
+
+    public String getHeader() {
+        return header;
+    }
+
+    public String getProfileContent() {
+        return profileContent;
+    }
 }
+
